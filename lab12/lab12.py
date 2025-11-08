45a03e3811cfa3e98ca8f46428c32959df3697e7
Hamilton <h292623@dac.unicamp.br>
2025-11-08 11:51:21 +0000

    finalizando lab12

--- lab12/lab12.py
+++ lab12/lab12.py
@@ -6,55 +6,72 @@
 ###################################################
 
 # Leitura da horta
-horta_linhas = int(input()) # número de linhas da matriz que representa a horta
+altura_horta = int(input()) # número de linhas da matriz que representa a horta
 matriz_horta = [] # matriz que representa a horta
-for linha in range(horta_linhas):
-    matriz_horta.append(input()) # recebe as entradas ("." ou "#") da matriz_horta : "." indica uma posição livre e "# indica uma posição ocupada
+for linha in range(altura_horta):
+    matriz_horta.append(list(input())) # recebe as entradas ("." ou "#") da matriz_horta : "." indica uma posição livre e "# indica uma posição ocupada
 
+altura_horta = len(matriz_horta)
+largura_horta = len(matriz_horta[0])
 
 # Leitura e posicionamento de cada planta
-def PosicionaPlanta(tipo_planta, matriz_planta):
-    quantidade_linhas_planta = len(matriz_planta)
-    quantidade_colunas_planta = len(matriz_planta[0])
-    quantidade_linhas_horta = len(matriz_horta)
-    quantidade_colunas_horta = len(matriz_horta[0])
-    matriz_provisoria = []
-    for linha_horta in range(matriz_horta):
-        matriz_provisoria.append(linha_horta.copy())
+def VerificaSeCabe(matriz_horta, matriz_planta, linha_ancora, coluna_ancora, altura_planta, largura_planta):
 
-    if tipo_planta == "P": # "P" é a planta de solo: linha mais baixa e posição mais à esquerda
-        for linha_horta in range(horta_linhas, 0, -1):
-            for coluna_horta in range(len(matriz_horta[linha_horta]), 0, -1):
-                if matriz_horta[linha_horta][coluna_horta] == ".":
-                    linhas_provisorias = quantidade_linhas_horta - quantidade_linhas_planta
-                    colunas_provisorias = quantidade_colunas_horta - quantidade_colunas_planta
-                    for linhas_apagadas in range(linhas_provisorias):
-                        for colunas_apagadas in range(colunas_provisorias, 0, -1):
-                            del matriz_provisoria[linhas_apagadas]
-                            del matriz_provisoria[linhas_apagadas][colunas_apagadas]
-                    print("\n", matriz_provisoria)  
-                print("\n", matriz_provisoria)
-            print("\n", matriz_provisoria)
-        print("\n", matriz_provisoria)   
-        return
+    for linha_planta in range(altura_planta):
+        for coluna_planta in range(largura_planta):
+            linha_na_horta = linha_ancora + linha_planta
+            coluna_na_horta = coluna_ancora + coluna_planta
 
-    if tipo_planta == "L": # "L" é a planta de altitude: linha mais alta e posição mais à esquerda
+            if "#" == matriz_planta[linha_planta][coluna_planta]:
+                if not (0 <= linha_na_horta < altura_horta and 0 <= coluna_na_horta < largura_horta):
+                    return False
+                    
+                if "#" == matriz_horta[linha_na_horta][coluna_na_horta]:
+                    return False
 
+    return True
 
-        return
+def PlantaNaHorta(matriz_horta, matriz_planta, linha_ancora, coluna_ancora, altura_planta, largura_planta):
 
-# looping 
+    for linha_planta in range(altura_planta):
+        for coluna_planta in range(largura_planta):
+            if "#" == matriz_planta[linha_planta][coluna_planta]:
+                linha_na_horta = linha_ancora + linha_planta
+                coluna_na_horta = coluna_ancora + coluna_planta
+                if (0 <= linha_na_horta < altura_horta and 0 <= coluna_na_horta < largura_horta):
+                    matriz_horta[linha_na_horta][coluna_na_horta] = "#"
+    
+    return matriz_horta
 
-# leitura
-planta_linhas = int(input()) #número de 'linhas' da planta, i.e. tamanho vertical da planta
-matriz_planta = [] # matriz que representa o tamanho (as entradas) que a planta ocupa na horta
-for linha in range(planta_linhas):
-    matriz_planta.append(input()) # recebe as entradas ("." ou "#") do matriz_planta : "." indica uma posição livre e "# indica uma posição ocupada
-tipo_planta = str(input()) # recebe o tipo da planta ("P" = planta de solo ou "L" = planta de altitude) 
+def PosicionaPlanta(matriz_horta, matriz_planta, tipo_planta, altura_planta, largura_planta):
 
-# processamento
-PosicionaPlanta(tipo_planta, matriz_planta)
+    if "P" == tipo_planta:
+        varia_range = range(altura_horta - 1, -1, -1)
+    else: # "L"
+        varia_range = range(altura_horta)
+
+    for linha_ancora in varia_range:
+        for coluna_ancora in range(largura_horta):
+            if VerificaSeCabe(matriz_horta, matriz_planta, linha_ancora, coluna_ancora, altura_planta, largura_planta):
+                matriz_horta = PlantaNaHorta(matriz_horta, matriz_planta, linha_ancora, coluna_ancora, altura_planta, largura_planta)
+                return matriz_horta 
+
+    return matriz_horta
+
+quantidade_plantas = int(input())# número de plantas que irão ser colocadas na horta
+for planta in range(quantidade_plantas):
+    planta_linhas = int(input()) # número de 'linhas' da planta, i.e. tamanho vertical da planta
+    matriz_planta = [] # matriz que representa o tamanho (as entradas) que a planta ocupa na horta
+    for linha in range(planta_linhas):
+        matriz_planta.append(list(input())) # recebe as entradas ("." ou "#") do matriz_planta : "." indica uma posição livre e "# indica uma posição ocupada
+    
+    altura_planta = len(matriz_planta)
+    largura_planta = len(matriz_planta[0])
+
+    tipo_planta = str(input()) # recebe o tipo da planta ("P" = planta de solo ou "L" = planta de altitude)
+    matriz_horta = PosicionaPlanta(matriz_horta, matriz_planta, tipo_planta, altura_planta, largura_planta)
 
 
 # Impressão da horta
-print(matriz_horta)
+for linha in matriz_horta:
+    print("".join(linha))
