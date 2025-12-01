d05b7e74ebf0453642f6d643a9b734dfd92b60b5
Hamilton <h292623@dac.unicamp.br>
2025-12-01 00:54:07 +0000

    iniciando lab14 e lab15

---
+++ lab14/lab14.py
@@ -0,0 +1,32 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 14 - Sintetizador de DNA
+# Nome:
+# RA:
+###################################################
+
+"""
+Função para verificar se é possível sintetizar uma sequência de DNA,
+respeitando o custo máximo.  Os argumentos correspondem à
+sequência_atual, à sequência_final desejada, ao custo_atual e ao
+orçamento total (MAX_CUSTO). A função deve retornar se é possível
+sintetizar a sequência desejada (True/False), respeitando o orçamento.
+
+Os argumentos e o retorno dessa função podem ser alterados, mas a
+função deve ser implementada de forma recursiva.
+
+IMPORTANTE: A submissão de um programa sem uma FUNÇÃO RECURSIVA válida
+            implementada será considerada TENTATIVA DE FRAUDE.
+
+"""
+
+def sintetizadorDNA(sequencia_atual, sequencia_final, custo_atual, MAX_CUSTO):
+    return True
+
+    return
+
+# Leitura de dados
+
+# Processamento da sequência
+
+# Impressão de saída
---
+++ lab15/lab15.py
@@ -0,0 +1,34 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 15 - Joomba II
+# Nome: 
+# RA: 
+###################################################
+
+"""
+Função que determina se todas as janelas podem ser limpas, essa
+função deve ser implementada de forma recursiva. A função recebe
+uma estrutura predio representando o prédio, a posição (f,a,j) do
+Joomba, o número n de comandos restantes e o último comando c
+executado. A função deve retornar se é possível limpar todas as
+janelas com os comandos restantes. Os argumentos e o retorno dessa
+função podem ser alterados, mas a função deve ser implementada de
+forma recursiva.
+
+IMPORTANTE: A submissão de um programa sem uma FUNÇÃO RECURSIVA válida
+            implementada será considerada TENTATIVA DE FRAUDE.
+"""
+
+
+def checar_limpeza(predio, f, a, j, n, c):
+
+    return
+ 
+# Leitura da entrada
+
+
+# Chamada da função e impressão do resultado, o 'X' indica que não houve nenhum comando anterior.
+if checar_limpeza(predio, 'N', 0, 1, N, 'X'):
+  print("Comandos encontrados")
+else:
+  print("Comandos não encontrados")
